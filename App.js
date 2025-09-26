bc259e25733e84c38a82e375db0d322974e38f5e
shaineanana <shaineanana@gmail.com>
2025-09-26 10:54:06 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import CounterApp from './CounterApp';
+import ColorChangerApp from './ColorChangerApp';
+import React from 'react';
+
+export default function App() {
+  return (
+    <>
+      <CounterApp/>
+      <ColorChangerApp/>
+    </>
+  )
+}
