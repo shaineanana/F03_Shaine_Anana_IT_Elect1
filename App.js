99df56d8a1a12f83c1194e58583bc0894960f548
shaineanana <shaineanana@gmail.com>
2025-09-26 11:00:34 +0800

    App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import ChatMessage from './ChatMessage';
+import CommentSection from './CommentSection';
+import React from 'react';
+
+export default function App() {
+  return (
+    <>
+      <ChatMessage/>
+      <CommentSection/>
+    </>
+  )
+}
